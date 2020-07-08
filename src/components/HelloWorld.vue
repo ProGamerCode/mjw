ceff757e3004b2ed1da4e880b7d05b3e397d2f74
ProGamerCode <progamercode@outlook.com>
2020-07-08 08:56:02 -0700

    homepage layout done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,51 +10,61 @@
         <a class="button is-link" href="https://rpubs.com/ProGamerCode/630250">White Paper</a>
       </div>
    </article>
-    <div class="columns is-primary is-gapless is-multiline">
+<div class="columns is-primary is-gapless is-multiline">
+      <figure class="image is-128x128">
+        <img src="../assets/trampstamp.png">
+      </figure>
       <article class="message is-danger">
         <div class="message-body">
-        <h2>I know what your all thinking <i>Oh boy,Yet Another ERC20 token</i></h2>
+          <h2>I know what your all thinking <i>Oh boy,Yet Another ERC20 token</i></h2>
         </div>
-      </article>
+     </article>
+        <figure class="image is-128x128">
+          <img src="../assets/trampstamp.png">
+        </figure>
       <article class="message is-warning">
         <div class="message-body">
           <h3><b> So without reading the technical WP heres all you need to know in neat bundles below</b></h3>
         </div>      
       </article>
+        <figure class="image is-128x128">
+          <img src="../assets/trampstamp.png">
+        </figure>
       <div class="gap"></div>
       <div class="column">
         <article class="message is-dark">
           <div class="message-header">
-            <p>Dark</p>
+            <p class="title has-text-warning">Cannabis Data Incentivised</p>
             <button class="delete" aria-label="delete"></button>
           </div>
           <div class="message-body">
-             Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+             Record your Marijuana related transactions by weight. Each data entry is also an entry into a weed raffle/lottery payed out monthly automated direct fair with opprotunity to marijuana based businesses and average consumers both to increase their odds and therefore winnings 
           </div>
         </article>
       </div>
       <div class="column">
         <article class="message is-success">
           <div class="message-header">
-            <p>Success</p>
+            <p class="title has-text-dark">A 420Friendly Rideshare</p>
             <button class="delete" aria-label="delete"></button>
           </div>
           <div class="message-body">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+           Providing our userbase with the worlds first 420 friendly auction based rideshare Dapp where Drivers can look for customer offering bids riders can in our service enter destination addresses or draw their route on their map location data when submiting offers
           </div>
         </article>
       </div>
       <div class="column">
         <article class="message is-primary">
           <div class="message-header">
-          <p>Info</p>
+          <p class="title has-text-white">Evolving Canna-Culture</p>
           <button class="delete" aria-label="delete"></button>
           </div>
           <div class="message-body">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+            Introducing the Mary J Watson smartshop standard(details in the WP). Cannabis culture from Farmer and Trimers to shop Owners and canabis consumers. All can and should benifit from this ecosystems structure. Allowing Hemp based businesses to thrive/grow digitaly    
           </div>
         </article>
     </div>
+<iframe width="760" height="515" src="https://www.youtube-nocookie.com/embed/A9i6rgbm40s" frameborder="0" allow="accelerometer; autoplay; encrypted-mediap; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   </div>
 </template>
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,8 @@
   <div class="about">
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
 
-    <h1>This is an about page</h1>
+    <h1 class="title">MJWC_WP</h1>
+
 
   </div>
 </template>
